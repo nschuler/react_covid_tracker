140024bc9365673ec1de612b29445a5a9d636d9e
nschuler <nicholas.schuler@student.uts.edu.au>
2020-12-22 18:44:42 +1100

    Enable Chart component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import { fetchData } from './api';
 import coronaImage from './images/image.png';
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -36,7 +35,7 @@ class App extends React.Component {
         <img className={styles.image} src={coronaImage} alt="COVID-19" />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange}/>
-        {/* <Chart data={data} country={country}/> */}
+        <Chart data={data} country={country}/>
       </div>
     );
   }
